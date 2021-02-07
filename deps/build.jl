b642ee1d271448e5e8b905c3f285135c56feb730
h0pery <anabel.ovide@gmail.com>
2021-02-07 15:37:47 +0200

    Bug in println

--- deps/build.jl
+++ deps/build.jl
@@ -1,13 +1,23 @@
-println("Second commit")
-println("Building package")
 arch = Sys.ARCH
-if Symbol("x86_64") == arch && (Sys.isunix() && Sys.islinux())
-    #pwd()
-    print("64")
-else
-    error("Not 64")
+
+function build(precision::Int, isWindows::Bool)::Nothing
+    #run(`git clone git@github.com:QuEST-Kit/QuEST.git && mkdir build && cd build`)
+    isWindows ? run(`cmake -DPRECISION=$precision .. -G "MinGW Makefiles"`) :
+                run(`cmake -DPRECISION=$precision ..`)
+    run(`make`)
+    #println("After make")
 end
 
-if Sys.isunix() && Sys.islinux()
-    println("cool!")
+if Symbol("x86_64") == arch && Sys.isunix()
+    cd("/home/hopery/Documents/job/testing/QuEST/build")
+    build(2, false)
+elseif Symbol("x86_64") == arch && Sys.iswindows()
+    build(2, true)
+elseif Symbol("x86_32") == arch && Sys.isunix()
+    build(1, false)
+elseif Symbol("x86_32") == arch && Sys.iswindows()
+    build(1, true)
+else
+    error("Architecture/OS not supported")
 end
+
