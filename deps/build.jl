105ce0b61d8a249b3f401c1dba761e91b54f1594
h0pery <anabel.ovide@gmail.com>
2021-02-07 16:24:52 +0200

    Build done!

--- deps/build.jl
+++ deps/build.jl
@@ -1,23 +1,29 @@
 arch = Sys.ARCH
 
 function build(precision::Int, isWindows::Bool)::Nothing
-    #run(`git clone git@github.com:QuEST-Kit/QuEST.git && mkdir build && cd build`)
-    isWindows ? run(`cmake -DPRECISION=$precision .. -G "MinGW Makefiles"`) :
-                run(`cmake -DPRECISION=$precision ..`)
-    run(`make`)
-    #println("After make")
+    run(`git clone https://github.com/QuEST-Kit/QuEST.git`)
+    cd("QuEST")
+    mkdir("build")
+    cd("build")
+
+    isWindows ? wait(run(`cmake -DPRECISION=$precision .. -G "MinGW Makefiles"`)) :
+                wait(run(`cmake -DPRECISION=$precision ..`))
+    wait(run(`make`))
 end
 
-if Symbol("x86_64") == arch && Sys.isunix()
-    cd("/home/hopery/Documents/job/testing/QuEST/build")
-    build(2, false)
-elseif Symbol("x86_64") == arch && Sys.iswindows()
-    build(2, true)
-elseif Symbol("x86_32") == arch && Sys.isunix()
-    build(1, false)
-elseif Symbol("x86_32") == arch && Sys.iswindows()
-    build(1, true)
-else
-    error("Architecture/OS not supported")
-end
+if !ispath("./QuEST") || isempty(readdir("./QuEST"))
+    println("Is empty!")
 
+    if Symbol("x86_64") == arch && Sys.isunix()
+        build(2, false)
+    elseif Symbol("x86_64") == arch && Sys.iswindows()
+        build(2, true)
+    elseif Symbol("x86_32") == arch && Sys.isunix()
+        build(1, false)
+    elseif Symbol("x86_32") == arch && Sys.iswindows()
+        build(1, true)
+    else
+        error("Architecture/OS not supported.")
+    end
+
+end
